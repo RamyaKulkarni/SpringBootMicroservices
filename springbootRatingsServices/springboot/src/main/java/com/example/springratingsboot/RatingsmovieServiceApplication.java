ffd71d14bc1b3af6623f62a12199bd90950572bd
Ramya Kulkarni <raku0517@netcracker.com>
2020-03-16 16:06:46 +0530

    Initial Setup

---
+++ springbootRatingsServices/springboot/src/main/java/com/example/springratingsboot/RatingsmovieServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springratingsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsmovieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsmovieServiceApplication.class, args);
+	}
+
+}
---
+++ springbootRatingsServices/springboot/src/test/java/com/example/springratingsboot/RatingsmovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springratingsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsmovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
